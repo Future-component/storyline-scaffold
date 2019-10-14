c2d542aa579ccfcc37d2e7a75b23ec1cab6676e3
BETH-zhang <522718277@qq.com>
2019-10-14 19:41:51 +0800

    添加额外的addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -4,3 +4,13 @@
 // import '@storybook/addon-notes/register-panel';
 // https://www.npmjs.com/package/@storybook/addon-notes
 // https://www.npmjs.com/package/@storybook/addon-info
+import '@storybook/addon-knobs/register';
+
+/*
+Info的提升版本readme.以及一键换背景的backgrounds。
+现成的Material-UI
+直接显示Jsx源码的Storybook-addon-jsx.
+控制版本显示的storybook-addon-versions，让你直接对比多个版本的区别。
+一键生成所有截图的Storybook Chrome Screenshot Addon。
+*/
+
--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
+import { withKnobs, number, object, boolean, text, select, date, array, color } from '@storybook/addon-knobs';
 
 storiesOf('Button', module)
   .add('with text', () => (
@@ -10,7 +11,26 @@ storiesOf('Button', module)
   })
   .add('with emoji', () => (
     <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  )); 
+  ))
+  .add('btn', () => {
+    const btnText = text('btnText', 'DIY Button'); //文字控件
+    const bold = boolean('Bold', false) //启用控件
+    const selectedBgColor = color('bgColor', '#fff');//颜色选择器
+    const selectedColor = color('Color', 'black');
+    const fontSize = number('fontSize', 20)//数字
+    const customStyle = object('Style', {
+      fontFamily: 'Arial',
+      padding: "20px",
+    });
+    const style = {
+      ...customStyle,
+      fontWeight: bold ? 800 : 400,
+      fontSize: fontSize + 'px',
+      color: selectedColor,
+      backgroundColor: selectedBgColor
+    };
+    return  <Button style={style}>{btnText}</Button>
+  })
   
 storiesOf('测试物料', module)
 .add('Test', () => (<div>
